b08631b131568ca19f4ae50b81494332a163a206
Nurhanianzah <ipungnurhanianzah17@gmail.com>
2023-11-15 14:24:59 +0700

    ah

--- smartoffice/lib/main.dart
+++ smartoffice/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:smartoffice/homepage.dart';// Sesuaikan dengan lokasi file halaman home Anda
+import 'package:smartoffice/homepage.dart'; // Sesuaikan dengan lokasi file halaman home Anda
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:HomePage(),
+      home: HomePage(),
     ),
   );
 }
